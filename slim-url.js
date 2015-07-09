7474c34f58321591443a61a145bd23ed21f622f4
Victor <runner62v6@gmail.com>
2015-07-10 00:41:20 +0300

    fixed url with hash

--- slim-url.js
+++ slim-url.js
@@ -6,22 +6,25 @@ URL.prototype.toString = function() {
     return this.t;
 }
 
-URL.prototype.get = function(arg) {
-    if(this.contains(arg))
-        return this.t.split(arg+"=")[1].split("&")[0];
+URL.prototype.get = function(a) {
+    if(this.contains(a))
+        return this.t.split(a+"=")[1].split(/[&#]/)[0];
 }
 
-URL.prototype.contains = function(arg) {
-    return this.t.search(arg+"=")>-1;
+URL.prototype.contains = function(a) {
+    return this.t.search(a+"=")>-1;
 }
 
 URL.prototype.set = function(k, v) {
     var i = this.t;
     if(this.contains(k)){
-        i = i.replace(k+"="+i.split(k+"=")[1].split("&")[0], k+"="+v);
+        i = i.replace(k+"="+i.split(k+"=")[1].split(/[&#]/)[0], k+"="+v);
     } else {
+        var o = i.split("#");
+        i = o[0];
         i += i.search("\\?")>-1 ? "&" : "?";
         i += k + "=" + v;
+        o.length == 1 ? i : i+="#"+o[1];
     }
     this.t = i;
     return this;
